f12382f674c8645050ca6df034b6748c4a2f6efd
etienne robaglia <etienne.robaglia@ext.mpsa.com>
2021-08-05 16:55:25 +0700

    add 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,12 @@
-Cette page n'existe pas!
+<?php get_header(); ?>
+
+<main class="page-container">
+    <h1 class="page-titre" style="display: block;">
+            Cette page n'existe pas!
+    </h1>        
+    <section class="page-text">
+            <?php echo the_content(); ?>
+    </section>
+</main>
+
+<?php get_footer(); ?>

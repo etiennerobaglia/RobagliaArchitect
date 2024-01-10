b72e3d8057b6db004bf95f2c2aee1602caeb9183
etienne robaglia <e.robaglia@gmail.com>
2024-01-10 14:10:46 +0700

    fix function.php and update deploy

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Load custom CSS and JavaScript.
- */
 add_action( 'wp_enqueue_scripts', 'wpdocs_my_enqueue_scripts' );
 function wpdocs_my_enqueue_scripts() : void {
     wp_enqueue_script( 'main', get_template_directory_uri().'/assets/js/main.js');
